28c07f8e2a0598003f7e4f5d64d00bb25042418e
ameen-browserstack <ameen@browserstack.com>
2015-07-10 17:42:02 +0530

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative "rover.rb"
+
+n, m = gets.chomp.split
+p = Plateau.new(n,m)
+while line = gets
+	x,y,direction = line.split
+	cmd = gets.chomp
+	r = Rover.new(x,y,direction,p,cmd)
+	p.add_rover(r)
+end
+
+p.rovers.each do |r|
+	r.execute
+	puts "#{r.x} #{r.y} #{r.direction}"
+end
+
