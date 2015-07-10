bbba252ff0fda465aaa946896b4230c8bc439904
ameen-browserstack <ameen@browserstack.com>
2015-07-10 17:49:36 +0530

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,14 +3,13 @@ require_relative "rover.rb"
 n, m = gets.chomp.split
 p = Plateau.new(n,m)
 while line = gets
-	x,y,direction = line.split
-	cmd = gets.chomp
-	r = Rover.new(x,y,direction,p,cmd)
-	p.add_rover(r)
+  x,y,direction = line.split
+  cmd = gets.chomp
+  r = Rover.new(x,y,direction,p,cmd)
+  p.add_rover(r)
 end
 
 p.rovers.each do |r|
-	r.execute
-	puts "#{r.x} #{r.y} #{r.direction}"
+  r.execute
+  puts "#{r.x} #{r.y} #{r.direction}"
 end
-
